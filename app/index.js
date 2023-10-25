ee621028bc85cfa6b80294b48ce31c57d020897b
Tan Ling Jen <tanlingjen.tlj@gmail.com>
2023-10-25 22:16:02 +0800

    first commit eg2606b proj

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native';
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    )
+}
